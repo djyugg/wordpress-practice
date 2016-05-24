77d316dc652141f5aed9edf188964cafc2dd7609
djyugg <sv03@n741x1.com>
2016-05-25 01:26:07 +0900

    Add apache2 configuration process

--- install.sh
+++ install.sh
@@ -3,9 +3,11 @@ sudo apt-get update
 sudo apt-get upgrade
 
 
-sudo apt-get install php5 php5-mysql mysql-server apache
-
+sudo apt-get install php5 php5-mysql mysql-server apache2
+sudo a2enmod rewrite
+sudo a2enmod expires
 chown www-data:www-data -R  /var/www/html
+sudo service apache2 restart
 
 cd /var/www/html
 
